4a61b65e2a1d9e11e2168511c563f9037c5c9085
BartoszBuko <bukobartosz@gmail.com>
2023-04-06 17:50:06 +0000

    added new pages,layouts and images

---
+++ public/js/modal.js
@@ -0,0 +1,30 @@
+// modal functionality
+
+// Get the modal
+var modal = document.querySelector("[data-modal]");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var images = document.querySelectorAll("[data-image]");
+var modalImg = document.querySelector("[data-modal-img]");
+var captionText = document.querySelector("[data-modal-caption]");
+
+images.forEach((image, i) => {
+  image.addEventListener("click", (e) => {
+    console.log(e.target.attributes[2].value);
+    modalImg.src = `../img/${e.target.attributes[2].value}.webp`;
+
+    modal.style.display = "block";
+    body.classList.add("prevent-scroll");
+    html.classList.add("prevent-scroll");
+  });
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+  body.classList.remove("prevent-scroll");
+  html.classList.remove("prevent-scroll");
+};
